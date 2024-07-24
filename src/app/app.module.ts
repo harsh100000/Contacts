127276ebc367b2af7f69165939bc422a6afc9ea4
harsh_vardhan <harsh10112001@gmail.com>
2024-07-24 21:58:27 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TempComponent } from './temp/temp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TempComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
